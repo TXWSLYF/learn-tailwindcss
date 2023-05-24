fc0bd03dabe10443892e57809fcbc85d4289e5d9
alex <luyufeng@ginzaxiaoma.com>
2023-05-24 10:22:37 +0900

    feat: modal 动效完美响应式布局

--- app/component/page.tsx
+++ app/component/page.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 
 const Page = () => {
   const [open, setOpen] = useState(false);
+  const [open2, setOpen2] = useState(false);
 
   return (
     <div>
@@ -80,6 +81,24 @@ const Page = () => {
         }}
       >
         modal content
+        <Button onClick={() => {
+          setOpen2(true)
+        }}>
+          打开第二个弹窗
+          
+        </Button>
+        <Modal
+            open={open2}
+            title="ショッピングバッグ"
+            onOk={() => {
+              setOpen2(false);
+            }}
+            onCancel={() => {
+              setOpen2(false);
+            }}
+          >
+            我是第二个弹窗
+          </Modal>
       </Modal>
     </div>
   );
